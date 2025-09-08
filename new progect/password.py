026b27e374f4a50e6c5a58f06b28b4ec5f0d176a
NT-SAY <syroeginvita@gmail.com>
2025-09-08 17:38:30 +0300

    Add files via upload

--- new progect/password.py
+++ new progect/password.py
@@ -5,8 +5,21 @@ def Random_password(num):
         raise ValueError("you need more password")
     password = secrets.token_urlsafe(num)
     return password
-def heshing_password(hm):
-    num1 = secrets.token_bytes(hm)
-    return num1
-mage = Random_password(8)
-print(mage)
+
+
+def heshing_password_alg(password, name_alg):
+    print(f"alogoritm heshing{hashlib.algorithms_available}")
+    if name_alg not in hashlib.algorithms_available:
+        raise ValueError(f"Algorithm {name_alg} not available")
+    
+    hash_obj = hashlib.new(name_alg)
+    hash_obj.update(password.encode())
+    return hash_obj.hexdigest()
+
+
+password1 = Random_password(11)
+print(f"your password: {Random_password(9)}")
+print(f"alogoritm heshing{hashlib.algorithms_available}")
+name_alg = input('enter your alg: ')
+hashing_password = heshing_password_alg(password1, name_alg)
+print(hashing_password)
